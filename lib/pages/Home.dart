8e67bc8324d2281710613c256f8b7dbc0d85e88e
samgyngobr <samuel0360@gmail.com>
2019-07-24 18:34:01 -0300

    .

---
+++ lib/pages/Home.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatefulWidget
+{
+  Home({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+
+class _HomeState extends State<Home> {
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+
+      appBar: AppBar( title: Text("Home") ),
+
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: <Widget>[
+
+
+
+            ]
+          ),
+        )
+      ),
+
+
+    );
+  }
+}
